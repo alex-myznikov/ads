347d6c5dee76e6facfe5590b3fea15b25b168da7
Alexander Myznikov <post@edev-service.ru>
2020-01-08 19:51:57 +0300

    build-config: Add pre and post publish scripts.

---
+++ tools/clear-publication-artifacts.js
@@ -0,0 +1,8 @@
+const fs = require('fs-extra');
+const path = require('path');
+const cachedManifestFile = path.resolve(__dirname, '../cached-package.json');
+const manifestFile = path.resolve(__dirname, '../package.json');
+const aliases = ['lists', 'maps', 'queues', 'searches', 'text-processing', 'trees'];
+
+aliases.forEach(alias => fs.removeSync(path.resolve(__dirname, `../${alias}`)));
+fs.moveSync(cachedManifestFile, manifestFile, { overwrite: true });
---
+++ tools/prepare-package.js
@@ -0,0 +1,29 @@
+const fs = require('fs-extra');
+const path = require('path');
+const cachedManifestFile = path.resolve(__dirname, '../cached-package.json');
+const manifestFile = path.resolve(__dirname, '../package.json');
+const aliases = ['lists', 'maps', 'queues', 'searches', 'text-processing', 'trees'];
+const devManifestFields = ['scripts', 'devDependencies', 'nyc'];
+
+// Prepare aliases to import sources from 'ads-js/<source_group_name>'
+aliases
+  .forEach(alias => {
+    const aliasPath = path.resolve(__dirname, `../${alias}`);
+
+    if (fs.existsSync(aliasPath)) fs.removeSync(aliasPath);
+    fs.ensureDirSync(aliasPath);
+    fs.writeJSONSync(`${aliasPath}/package.json`, {
+      name: `ads-js/${alias}`,
+      main: `../dist/cjs/${alias}/index.js`,
+      module: `../dist/esm/${alias}/index.js`,
+      types: `../dist/types/${alias}/index.d.ts`,
+    }, { spaces: 2 });
+  });
+
+// Strip unnecessary sections out of published package.json version
+fs.copyFileSync(manifestFile, cachedManifestFile);
+Object.entries(fs.readJSONSync(manifestFile)).reduce((acc, cur) => {
+  if (!devManifestFields.includes(cur[0])) acc[cur[0]] = cur[1];
+
+  return acc;
+}, {});
